46232c361179f438caf6a00b3ee783263df3ec5e
lirik <kirill.isachenko@gmail.com>
2023-01-24 09:01:49 +0700

    @ path for vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   base: '/easytolearn/',
   plugins: [react()],
   server: {
