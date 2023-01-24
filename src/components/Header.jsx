afe586ff3ff94532ac6fb7c8ff9488f7396858b7
lirik <kirill.isachenko@gmail.com>
2023-01-24 09:45:16 +0700

    header logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import {
-  Box,
-  Flex,
-  Heading,
-} from '@chakra-ui/react';
-
+import React from "react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 
 function Header() {
   return (
-    <Box bg='white' px={4}>
-      <Flex h={16} alignItems={'center'} justifyContent={'center'}>
-        <Heading fontSize='2xl'>EasyToLearn</Heading>
+    <Box bg="white" px={4}>
+      <Flex h={16} alignItems={"center"} justifyContent={"center"}>
+        <Heading fontSize="2xl" color="teal.400" cursor='pointer'>
+          EasyToLearn
+        </Heading>
       </Flex>
     </Box>
   );
