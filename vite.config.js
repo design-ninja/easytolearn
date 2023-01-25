220b2bfff7e72bfcff9d3c1c6371f0140976a170
lirik <kirill.isachenko@gmail.com>
2023-01-25 09:37:01 +0700

    ghpages plugin added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { ghPages } from "vite-plugin-gh-pages";
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   server: {
     watch: {
       usePolling: true,
